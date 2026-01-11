ff39a7ae7fd9ddf8198635b634668f91799162f6
Zero <ZERO_BROTHER12345@outlook.com>
2026-01-11 11:16:43 +0100

    new work

--- Py05/ex1/data_stream.py
+++ Py05/ex1/data_stream.py
@@ -3,17 +3,80 @@ from typing import Any, List, Dict, Union, Optional
 
 
 class DataStream(ABC):
+    def __init__(self, stream_id: str, stream_type: str):
+        self.stream_id = stream_id
+        self.stream_type = stream_type
+
 
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
-
-    @abstractmethod
-    def filter_data(
-        self, data_batch: List[Any], criteria: Optional[str] = None
-    ) -> List[Any]:
+ 
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
         pass
 
-    @abstractmethod
-    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+    def  get_stats(self) -> Dict[str, Union[str, int, float]]:
         pass
+
+
+
+class SensorStream(DataStream):
+    def __init__(self, stream_id, stream_type):
+        super().__init__(stream_id, stream_type)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        new_lst = [f"{key}: {val}" for key, val in data_batch[0].items()]
+        return f"Processing sensor batch: {new_lst}"
+ 
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        
+
+    def  get_stats(self) -> Dict[str, Union[str, int, float]]:
+        
+        
+
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id, stream_type):
+        super().__init__(stream_id, stream_type)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        new_lst = [f"{key}: {val}" for key, val in data_batch[0].items()]
+        readable_str = str(new_lst).replace("'" ,"")
+        return f"Processing sensor batch: {readable_str}"
+ 
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        
+
+    def  get_stats(self) -> Dict[str, Union[str, int, float]]:
+        
+
+    
+
+class EventStream(DataStream):
+    def __init__(self, stream_id, stream_type):
+        super().__init__(stream_id, stream_type)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        readable_str = str(data_batch).replace("'" ,"")
+        return f"Processing sensor batch: {readable_str}"
+ 
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        
+
+    def  get_stats(self) -> Dict[str, Union[str, int, float]]:
+        
+        
+
+
+class StreamProcessor():
+
+
+
+if __name__ == "__main__":
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
+
